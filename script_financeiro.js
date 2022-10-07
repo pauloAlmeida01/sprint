f8f96bb509b8926e2db033114b5349f0fd548616
leonardosantosborges <leonardo.borges@sptech.school>
2022-10-07 14:51:55 -0300

    scriptSimulador att

--- script_financeiro.js
+++ script_financeiro.js
@@ -7,10 +7,18 @@ function calc() {
     var lucro3 = total * 3
     var lucro6 = total * 6
     var lucro10 = total * 10
-    p_msg.innerHTML = "O lucro com prejuízo de 19% é: R$ " + totalPreju.toFixed(2) + "<br>";
-    p_msg.innerHTML += "O lucro com a nossa solução é: R$ " + total.toFixed(2) + "<br>";
-    p_msg.innerHTML += `Após três(3) encomendas com a nossa solução seu lucro passa a ser de R$${lucro3.toFixed(2)} <br>`
-    p_msg.innerHTML += `Após seis(6) encomendas com a nossa solução seu lucro passa a ser de R$${lucro6.toFixed(2)} <br>`
-    p_msg.innerHTML += `Após dez(10) encomendas com a nossa solução seu lucro passa a ser de R$${lucro10.toFixed(2)}`
+   
+
+    if (qtdAlface <= 0) {
+        alert(`Insira um valor maior que 0.`)   
+    } else if (isNaN(qtdAlface)) {
+        alert(`Insira um valor válido!`)
+    } else {
+        p_msg.innerHTML = "O lucro com prejuízo de 19% é: R$ " + totalPreju.toFixed(2) + "<br>";
+        p_msg.innerHTML += "O lucro com a nossa solução é: R$ " + total.toFixed(2) + "<br>";
+        p_msg.innerHTML += `Após três(3) encomendas com a nossa solução seu lucro passa a ser de R$${lucro3.toFixed(2)} <br>`
+        p_msg.innerHTML += `Após seis(6) encomendas com a nossa solução seu lucro passa a ser de R$${lucro6.toFixed(2)} <br>`
+        p_msg.innerHTML += `Após dez(10) encomendas com a nossa solução seu lucro passa a ser de R$${lucro10.toFixed(2)}`
+    }
     
 }
