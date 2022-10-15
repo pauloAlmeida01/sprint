acd03194ed4ee75669d739e74a93a493dc66f71e
alanaquinoslv <alan.silva@sptech.school>
2022-10-15 15:41:59 -0300

    modal adicionado

---
+++ site/public/js/scriptmodais.js
@@ -0,0 +1,21 @@
+const modal = document.querySelector('.modal-container')
+
+function openModal() {
+  modal.classList.add('active')
+}
+
+function closeModal() {
+  modal.classList.remove('active')
+}
+
+
+const modal2 = document.querySelector('.modal2-container')
+
+function openModal2() {
+  modal.classList.add('active')
+}
+
+function closeModal2() {
+  modal.classList.remove('active')
+}
+
