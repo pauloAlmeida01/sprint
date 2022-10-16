f14b16ff38afb925ad884298a8c7c9f8c2eede39
LucasSouza1407 <lucas.milagres@sptech.school>
2022-10-16 00:41:12 -0300

    Alterações no DASHBOARD (gráficos pop-ups)

--- site/public/js/scriptmodais.js
+++ site/public/js/scriptmodais.js
@@ -9,14 +9,36 @@ function closeModal() {
 }
 
 
-const modal2 = document.querySelector('.modal2-container')
+const modal2 = document.querySelector('.modal-container2')
 
 
 function openModal2() {
-  modal.classList.add('active')
+  modal2.classList.add('active')
 }
 
 function closeModal2() {
-  modal.classList.remove('active')
+  modal2.classList.remove('active')
+}
+
+const modal3 = document.querySelector('.modal-container3')
+
+
+function openModal3() {
+  modal3.classList.add('active')
+}
+
+function closeModal3() {
+  modal3.classList.remove('active')
+}
+
+const modal4 = document.querySelector('.modal-container4')
+
+
+function openModal4() {
+  modal4.classList.add('active')
+}
+
+function closeModal4() {
+  modal4.classList.remove('active')
 }
 
