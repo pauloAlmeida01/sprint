fd8b213a132ba1c80c4eb6a111eb65ae03b23bb9
gabrielfernando23 <gabriel.ssilva@sptech.school>
2022-10-17 21:43:11 -0300

    correção do script de validação

--- site/public/js/scriptCadastro-2.js
+++ site/public/js/scriptCadastro-2.js
@@ -12,21 +12,19 @@ function cadastrar2() {
     else if (cep.length < 8 || cep.length > 8) {
         alert('O CEP deve conter 8 números')
     }
-
-    if (numero == '') {
+    else if (numero == '') {
         alert('Digite o número!')
     }
     else if (isNaN(numero)) {
         alert('Por favor digite um número válido!')
     }
-
-    if (logradouro == '') {
+    else if (logradouro == '') {
         alert('Digite o Logradouro!')
     }
-    if (complemento == '') {
+    else if (complemento == '') {
         alert('Digite o complemento!')
     }
-    if (cidade == '') {
+    else if (cidade == '') {
         alert('Digite sua Cidade!')
     }
     else{alert('Cadastro efetuado com sucesso!')
--- site/public/js/scriptCadastro.js
+++ site/public/js/scriptCadastro.js
@@ -30,34 +30,39 @@ const senha = document.querySelector('.senha');
         var confirmacao = ipt_confirmSenha.value
         var email = ipt_email.value
         var campos_validos = 'no'
-        if (telefone == '') {
+        if (nome == '') {
+            alert('Digite seu nome!')
+        }
+        else if (telefone == '') {
             alert('Você precisa digitar o seu telefone!')
         }
-        if (cnpj == '') {
-            alert('Você precisa digitar seu CNPJ!')
-        }
-        if (senha == '') {
-            alert('Digite sua senha.')
-        }
-        if (confirmacao == '') {
-            alert('Não se esqueça de confirmar sua senha.')
-        }
-        if (email == '') {
-            alert('Digite o seu email')
-        }
-        if (telefone.length < 10 || telefone.length > 10) {
+        else if (telefone.length < 10 || telefone.length > 10) {
             alert('Digite um número de telefone válido!')
         }
-        if (cnpj.length < 14 || cnpj.lenght > 14) {
+        else if (cnpj == '') {
+            alert('Você precisa digitar seu CNPJ!')
+        }
+        else if (email == '') {
+            alert('Digite o seu email')
+        }
+        else if (!email.includes("@") && !email.includes(".")) {
+            alert('Digite um email válido!')
+        }
+        else if (senha == '') {
+            alert('Digite sua senha.')
+        }
+        else if (confirmacao == '') {
+            alert('Não se esqueça de confirmar sua senha.')
+        }
+        else if (cnpj.length < 14 || cnpj.lenght > 14) {
             alert('Digite um CNPJ válido!')
         }
-        if (senha != confirmacao) {
+        else if (senha != confirmacao) {
             alert('A senha e a confirmação não coincidem!')
         }
-        if (email.includes("@") && email.includes(".")) {
-            campos_validos = 'yes';
+        else{
+            campos_validos = 'yes'
         }
-        else{alert('Digite um email válido!')}
         
         if (campos_validos == 'yes') {
             window.location.replace("cadastro-2.html")
