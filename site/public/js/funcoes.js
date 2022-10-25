41a9ad55a8b53a672c2e2014c9d4d4616af2e71b
pauloAlmeida01 <paulo.rsantos@sptech.school>
2022-10-25 20:18:51 -0300

    Alteração nos links

--- site/public/js/funcoes.js
+++ site/public/js/funcoes.js
@@ -21,7 +21,7 @@ function limparSessao() {
     // aguardar();
     sessionStorage.clear();
     // finalizarAguardar();
-    window.location = "../login.html";
+    window.location = "../Index.html";
 }
 
 // carregamento (loading)
